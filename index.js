795de663b09b820a9b8e9f11bd96b44b8de5bf15
skandog <muumrar@gmail.com>
2022-12-02 15:11:13 +0000

    part one complete - scoring logic built via for loop

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let input = `A Y
-B X
-C Z`;
+import input from "./input.js";
+
+// // Dummy input
+// let input = `A Y
+// B X
+// C Z`;
 
 let splinput = input.split("\n");
 
@@ -26,6 +29,18 @@ console.log(splinput.length);
 for (let i = 0; i < splinput.length; i++) {
   let oppoMove = moves[splinput[i].charAt(0)];
   let yourMove = moves[splinput[i].charAt(2)];
-
   console.log("oppo: ", oppoMove, "; player: ", yourMove);
+  totalScore += scores[yourMove];
+
+  if (yourMove == oppoMove) {
+    totalScore += 3;
+  } else if (yourMove === "Rock" && oppoMove === "Scissors") {
+    totalScore += 6;
+  } else if (yourMove === "Paper" && oppoMove === "Rock") {
+    totalScore += 6;
+  } else if (yourMove === "Scissors" && oppoMove === "Paper") {
+    totalScore += 6;
+  }
 }
+
+console.log(totalScore);
